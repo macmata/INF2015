10a5c3f168631d20c2aca7595f307da5e20b1ee9
Max <maximebelanger87@gmail.com>
2013-10-16 14:23:51 -0400

    Update json.py

--- builder/reader/json.py
+++ builder/reader/json.py
@@ -10,13 +10,13 @@ class JsonReader(object):
 
     def build_car(self):
         car = Car()
-        car.annee  = self.data["voitures"]["annee"]
-        car.marque = self.data["voitures"]["marque"]
-        car.modele = self.data["voitures"]["modele"]
-        car.valeur_des_options = self.data["voitures"]["valeur_des_options"]
-        car.burinage = self.data["voitures"]["burinage"]
-        car.garage_interieur = self.data["voitures"]["garage_interieur"]
-        car.systeme_alarme = self.data["voitures"]["systeme_alarme"]
+        car.annee  = self.data["voiture"]["annee"]
+        car.marque = self.data["voiture"]["marque"]
+        car.modele = self.data["voiture"]["modele"]
+        car.valeur_des_options = self.data["voiture"]["valeur_des_options"]
+        car.burinage = self.data["voiture"]["burinage"]
+        car.garage_interieur = self.data["voiture"]["garage_interieur"]
+        car.systeme_alarme = self.data["voiture"]["systeme_alarme"]
         return car
 
     def build_driver(self):
@@ -32,13 +32,13 @@ class JsonReader(object):
 
     def build_moto(self):
         moto = Moto()
-        moto.annee = ["motos"]["annee"]
-        moto.marque = ["motos"]["marque"]
-        moto.modele = ["motos"]["modele"]
-        moto.valeur_des_options = ["motos"]["valeur_des_options"]
-        moto.burinage = ["motos"]["burinage"]
-        moto.garage_interieur = ["motos"]["garage_interieur"]
-        moto.systeme_alarme = ["motos"]["systeme_alarme"]
+        moto.annee = ["moto"]["annee"]
+        moto.marque = ["moto"]["marque"]
+        moto.modele = ["moto"]["modele"]
+        moto.valeur_des_options = ["moto"]["valeur_des_options"]
+        moto.burinage = ["moto"]["burinage"]
+        moto.garage_interieur = ["moto"]["garage_interieur"]
+        moto.systeme_alarme = ["moto"]["systeme_alarme"]
         return moto
 
     def build_contrat(self):
@@ -52,4 +52,3 @@ class JsonReader(object):
                 self.build_car(),
                 self.build_contrat()
         )
-
