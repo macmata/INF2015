24585a112bc57a41b98c3f602e08de726e411c38
alex <info@alexandre-leblanc.com>
2013-11-16 15:41:34 -0500

    table creation

--- builder/stats/stats.py
+++ builder/stats/stats.py
@@ -1,27 +1,57 @@
-from quote import Quote
+#from quote import Quote
 import sqlite3
+from array import array
 
 
 
 
+class bdstats(objet):
+	def __init__(self,db_file):
+		
+
+	def creat_connection(self,db_file):
+		return connection = sqlite3.connect(db_file)
+
+	def create_cursor(self,connection):
+		return cursor = connection.cursor()
+
+	def create_table(self,cursor):
+		cursor.execute('''CREATE TABLE sousmission
+				     (id INTEGER PRIMARY KEY, gender TEXT, assurable int,  )''')
+		cursor.execute('''create TABLE vehicules (id INTEGER PRIMARY KEY,marque TEXT, soumission INTEGER REFERENCES SOUMISSION (id), type TEXT)''')
+	
+	def insert_toTable(self,cursor,quote):
+		cursor.execute("INSERT INTO stocks VALUES ()")
+	
+	def extract_qteModel(self,model):
+		#curseur.execute('SELECT * FROM Genre WHERE id=?', ids)
+
+	def prep_quote_forTable(self,quote):
+		prepQuote = []
+		if quote.cars.len < 3;
+			for i in quote.cars
+				prepQuote[i].append(quote.cars[i])
+			for i range(3- quote.cars.len):
+
+				prepQuote.append("null")
 
 
-conn = sqlite3.connect('statistiques.db')
-c = conn.cursor()
+				
 
-# Create table
-c.execute('''CREATE TABLE stats
-		             (homme INETEGER, femme INTERGER, nbVoiture INTERGER, nbMotos INTERGER, nbVoitures INTERGER,)''')
+			
+			
+		
 
-# Insert a row of data
-#c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
 
-# Save (commit) the changes
-conn.commit()
 
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
-conn.close()
+	def table_commit(self):
+		connection.commit()
+	
+	def table_close(self,connection):
+		connection.close()
+
+
+
 
 
 
