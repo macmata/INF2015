f1b0e9e0f57ce3aaac2c15023d92d4db3f8d0630
Maxime Belanger <belange87@hotmail.com>
2013-11-16 17:16:36 -0500

    test driver

---
+++ tests/driver.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import unittest
+import os
+from datetime import datetime, date
+
+from mock import mock_driver
+from builder    import quote
+
+class SuccessException(Exception):
+    pass
+
+class Driver(unittest.TestCase):
+
+    def setUp(self):
+        self.today = date.today()
+        self.driver = mock_driver(
+            datetime(
+                self.today.year - 51,
+                self.today.month,
+                self.today.day
+            ),
+            "Montréal",
+            "Québec",
+            "M",
+            date(self.today.year - 5, 1, 1),
+            True,
+            True,
+            True,
+        )
+
+    def test_age(self):
+        assert self.driver.age() == 51
+            
+    
+    def test_years_experience(self):
+        assert self.driver.years_experience() == 5
+
+
+if __name__ == '__main__':
+    unittest.main()
